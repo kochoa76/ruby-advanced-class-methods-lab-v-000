f60eea2a70781e82318efea9dbc507bc264c77d6
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-25 14:16:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,10 +33,7 @@ class Song
  end 
 
 def Song.find_or_create_by_name(name)
-  if self.name== name 
-    Song.find_by_name(name)
-    else 
-    Song.create_by_name(name)
+    Song.find_by_name(name) || Song.create_by_name(name)
   end
 end 
   
